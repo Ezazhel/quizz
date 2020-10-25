40932b7b44510b858e86730f7b736438000119b8
Ezazhel <Ezazhel@gmail.com>
2020-10-25 02:28:53 +0100

    Updates

---
+++ precache-manifest.a3a5b25a84c5a9b399bea6cdba2a6ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105550cd801d59dd228a7788900fe939",
+    "url": "/quizzdiscord/index.html"
+  },
+  {
+    "revision": "78eea1de06783ba5fbaa",
+    "url": "/quizzdiscord/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cfbd5e0afae60da7afe9",
+    "url": "/quizzdiscord/static/css/main.eb32c3f4.chunk.css"
+  },
+  {
+    "revision": "78eea1de06783ba5fbaa",
+    "url": "/quizzdiscord/static/js/2.f585e66d.chunk.js"
+  },
+  {
+    "revision": "65f5ddb972ec20d2f34381b91122a87f",
+    "url": "/quizzdiscord/static/js/2.f585e66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfbd5e0afae60da7afe9",
+    "url": "/quizzdiscord/static/js/main.b875d5b8.chunk.js"
+  },
+  {
+    "revision": "d443b399a99c79b31773",
+    "url": "/quizzdiscord/static/js/runtime-main.932986b8.js"
+  }
+]);
